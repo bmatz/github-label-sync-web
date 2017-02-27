2c6bc9b08a221c2efb4fe4c81ae3594727ae73df
Bernhard Matz <bernhard@trigo.at>
2017-02-27 13:14:19 +0100

    fixed main div to id app

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import ButtonCounter from './components/hello-world';
 
 ReactDOM.render(
-	<ButtonCounter />, document.querySelector('.app'));
+	<ButtonCounter />, document.getElementById('app')
+);
